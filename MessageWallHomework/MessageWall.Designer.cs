7803830f2cef29d73e89b74c81817ea3aea7878e
Josh <jblau2017@yahoo.com>
2025-09-19 08:30:12 -0600

    Add project files.

---
+++ MessageWallHomework/MessageWall.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace MessageWallHomework
+{
+    partial class MessageWall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.addMessage = new System.Windows.Forms.Button();
+            this.messageDisplay = new System.Windows.Forms.ListBox();
+            this.messagesDisplayBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(25, 61);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(158, 36);
+            this.firstNameLabel.TabIndex = 0;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(192, 58);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(226, 41);
+            this.firstNameText.TabIndex = 1;
+            this.firstNameText.TextChanged += new System.EventHandler(this.firstNameText_TextChanged);
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.Location = new System.Drawing.Point(651, 60);
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.Size = new System.Drawing.Size(204, 41);
+            this.lastNameText.TabIndex = 2;
+            this.lastNameText.TextChanged += new System.EventHandler(this.lastNameText_TextChanged);
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(489, 63);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(156, 36);
+            this.lastNameLabel.TabIndex = 3;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // addMessage
+            // 
+            this.addMessage.Location = new System.Drawing.Point(893, 60);
+            this.addMessage.Name = "addMessage";
+            this.addMessage.Size = new System.Drawing.Size(131, 47);
+            this.addMessage.TabIndex = 4;
+            this.addMessage.Text = "Add";
+            this.addMessage.UseVisualStyleBackColor = true;
+            this.addMessage.Click += new System.EventHandler(this.addMessage_Click);
+            // 
+            // messageDisplay
+            // 
+            this.messageDisplay.FormattingEnabled = true;
+            this.messageDisplay.ItemHeight = 36;
+            this.messageDisplay.Location = new System.Drawing.Point(31, 148);
+            this.messageDisplay.Name = "messageDisplay";
+            this.messageDisplay.Size = new System.Drawing.Size(824, 364);
+            this.messageDisplay.TabIndex = 5;
+            this.messageDisplay.SelectedIndexChanged += new System.EventHandler(this.messageDisplay_SelectedIndexChanged);
+            // 
+            // messagesDisplayBox
+            // 
+            this.messagesDisplayBox.AutoSize = true;
+            this.messagesDisplayBox.Location = new System.Drawing.Point(25, 109);
+            this.messagesDisplayBox.Name = "messagesDisplayBox";
+            this.messagesDisplayBox.Size = new System.Drawing.Size(150, 36);
+            this.messagesDisplayBox.TabIndex = 6;
+            this.messagesDisplayBox.Text = "Messages";
+            this.messagesDisplayBox.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MessageWall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.messagesDisplayBox);
+            this.Controls.Add(this.messageDisplay);
+            this.Controls.Add(this.addMessage);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.firstNameLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "MessageWall";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MessageWall_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.TextBox lastNameText;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Button addMessage;
+        private System.Windows.Forms.ListBox messageDisplay;
+        private System.Windows.Forms.Label messagesDisplayBox;
+    }
+}
+
---
+++ MessageWallHomework/MessageWall.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageWallHomework
+{
+    public partial class MessageWall : Form
+    {
+        BindingList<string> messages = new BindingList<string>();
+        public MessageWall()
+        {
+            InitializeComponent();
+            WireUpList();
+        }
+        private void WireUpList()
+        {
+            messageDisplay.DataSource = messages;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void firstNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lastNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void messageDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addMessage_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(messageDisplay.Text))
+            {
+                MessageBox.Show("Pleas enter a message before adding to the list.",
+                    "Blank Message Field", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                messages.Add(messageDisplay.Text);
+                messageDisplay.Text = "";
+            }
+            messageDisplay.Focus();
+
+        }
+
+        private void MessageWall_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
